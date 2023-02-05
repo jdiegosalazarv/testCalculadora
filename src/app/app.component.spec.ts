bef04185492ca6a3452f401dfb843d4e25716b06
Juan Diego Salazar Velasquez <diego.salazar@ias.com.co>
2023-02-05 14:48:57 -0500

    test funciones calculadora

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,30 +2,65 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    component = new AppComponent();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('El método sumar debe devolver la suma de numero1 + numero2', () => {
+    component.numero1 = 10;
+    component.numero2 = 5;
+    let result = 15;
+
+    expect(component.sumar()).toBe(result);
   });
 
-  it(`should have as title 'testCalc'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('testCalc');
+  it('El método restar debe devolver la resta de numero1 - numero2', () => {
+    component.numero1 = 10;
+    component.numero2 = 5;
+    let result = 5;
+
+    expect(component.restar()).toBe(result);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testCalc app is running!');
+  it('El método multiplicar debe devolver la multiplicación de numero1 * numero2', () => {
+    component.numero1 = 10;
+    component.numero2 = 5;
+    let result = 50;
+
+    expect(component.multiplicar()).toBe(result);
   });
+
+  it('El método multiplicar debe devolver 0 cuando numero1 o numero2 sea 0', () => {
+    component.numero1 = 10;
+    component.numero2 = 0;
+    let result = 0;
+
+    expect(component.multiplicar()).toBe(result);
+  });
+
+  it('El método dividir debe devolver la división de numero1 / numero2', () => {
+    component.numero1 = 10;
+    component.numero2 = 5;
+    let result = 2;
+
+    expect(component.dividir()).toBe(result);
+  });
+
+  it('El método dividir debe devolver 0 cuando numero1 0 numero2 sea igual a 0', () => {
+    component.numero1 = 10;
+    component.numero2 = 0;
+    let result = 0;
+
+    expect(component.dividir()).toBe(result);
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testCalc';
+  numero1: number = 0;
+  numero2: number = 0;
+
+  sumar() :number{
+    return this.numero1 + this.numero2;
+  }
+
+  restar(): number{
+    return this.numero1 - this.numero2;
+  }
+
+  multiplicar(): number{
+    return this.numero1 * this.numero2;
+  }
+
+  dividir(): number{
+    if (this.numero2 == 0) {
+      return this.numero2;
+    }
+    return this.numero1 / this.numero2;
+  }
 }
